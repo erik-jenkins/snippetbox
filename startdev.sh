5f9b94161a2c058ab9f226fe76778df920afc0dd
Erik Jenkins <ejenkins132@gmail.com>
2020-05-06 13:21:06 -0400

    add mac support to startdev

--- startdev.sh
+++ startdev.sh
@@ -3,6 +3,14 @@
 # This script will start mysql and adminer in a container and run the go server
 # locally.
 
+# determine how google chrome should be opened (OS-specific)
+launch_chrome_command=""
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  launch_chrome_command='/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --incognito "http://localhost:4000" "http://localhost:9001?username=root&db=snippetbox"'
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  launch_chrome_command='google-chrome --incognito "http://localhost:4000" "http://localhost:9001?username=root&db=snippetbox"'
+fi
+
 # prepare some cleanup stuff (stop docker)
 trap cleanup INT
 trap cleanup TERM
@@ -12,5 +20,5 @@ function cleanup() {
 }
 
 docker-compose -f docker-compose.yml up -V -d
-google-chrome --incognito "http://localhost:4000" "http://localhost:9001?username=root&db=snippetbox"
+eval $launch_chrome_command
 go run ./cmd/web
